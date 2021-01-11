386c9987672d099ca712ff5f2365fd210dcbd5b4
mrv96 <mrv96@users.noreply.github.com>
2021-01-11 16:41:37 +0100

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup (name = 'slp',
+       version = '0.1',
+       author = "tsmetana",
+       description = """SLP Library""",
+       py_modules = ["slp"],
+       packages=[''],
+       package_data={'': ['slp.so']},
+      )
